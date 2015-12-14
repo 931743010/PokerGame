fd2eda5c7d979a83abb72b7fdca4b767f3c38c71
qianfeng <qianfeng@sck.local>
2015-12-14 17:14:11 +0800

    创建

---
+++ 服务器端/ServerPoint/ServerPoint/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ServerPoint
+//
+//  Created by qianfeng on 15-12-14.
+//  Copyright (c) 2015年 sunck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
