d73054575a71bc4d39c4770865241c9e3f5c467b
qianfeng <qianfeng@sck.local>
2015-12-14 17:19:26 +0800

    客户端创建

---
+++ 客户端/ClientPoint/ClientPoint/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ClientPoint
+//
+//  Created by qianfeng on 15-12-14.
+//  Copyright (c) 2015年 sunck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
